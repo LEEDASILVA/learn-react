0c2f633f2ec2c0bd45c04337eab8b4c8762157dc
lee <lee-dasilva@hotmail.com>
2020-10-24 18:05:37 +0100

    more exercises

---
+++ todo/todo-list-part2/src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoItem from './todo-item'
+
+const App = () =>
+    (
+    <div className = "todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+    </div>
+    )
+
+
+export default App
---
+++ todo/todo-list-part2/src/components/todo-item.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoItem = () =>
+    (
+        <div className="todo-item">
+            <input type="checkbox" id="1" name="1" value="1"/>
+            <p>1</p>
+        </div>
+    )
+
+export default TodoItem
